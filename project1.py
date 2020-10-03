87a8e944bdc2aecb88be58cf0c0f00d2b3d2722e
roderick-whang <56379137+roderick-whang@users.noreply.github.com>
2020-10-03 15:26:32 -0400

    selection, insertion, merge sort

--- project1.py
+++ project1.py
@@ -3,21 +3,38 @@ Math 560
 Project 1
 Fall 2020
 
-Partner 1:
-Partner 2:
-Date:
+Partner 1: Sang-Jyh Lin
+Partner 2: Roderick Whang
+Date: 10/02/2020
 """
 
 """
 SelectionSort
 """
 def SelectionSort(listToSort):
+    for i in range(len(listToSort)):
+
+    # Find the minimum element
+        min_index = i
+        for j in range(i+1, len(listToSort)):
+            if listToSort[min_index] > listToSort[j]:
+                min_index = j
+    # Swap minimum element with the first element
+        listToSort[i], listToSort[min_index] = listToSort[min_index], listToSort[i]
     return listToSort
 
 """
 InsertionSort
 """
 def InsertionSort(listToSort):
+    for i in range(1, len(listToSort)):
+        k = listToSort[i] #set key element
+        j = i-1
+        # move element of listToSort[0..i-1]  if greater than key element
+        while j >=0 and listToSort[j] > k :
+            listToSort[j+1] = listToSort[j]
+            j -= 1
+        listToSort[j+1] = k
     return listToSort
 
 """
@@ -38,6 +55,42 @@ def BubbleSort(listToSort):
 MergeSort
 """
 def MergeSort(listToSort):
+
+    if len(listToSort) > 1:
+        mid = len(listToSort) // 2
+        left = listToSort[:mid]
+        right = listToSort[mid:]
+        #recursive call
+        MergeSort(left)
+        MergeSort(right)
+
+        i = 0 #left index
+        j = 0 #right index
+        k = 0 #listtosort index
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              #value from the left half has been used
+              listToSort[k] = left[i]
+              #move to the next slot
+              i += 1
+            else:
+                listToSort[k] = right[j]
+                #move to the next slot
+                j += 1
+            #move to the next slot
+            k += 1
+
+        #all the remaining values
+        while i < len(left):
+            listToSort[k] = left[i]
+            i += 1 #move to the next slot
+            k += 1 #move to the next slot
+
+        while j < len(right):
+            listToSort[k]=right[j]
+            j += 1 #move to the next slot
+            k += 1 #move to the next slot
     return listToSort
 
 """
