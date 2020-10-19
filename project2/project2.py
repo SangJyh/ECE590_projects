377d9f7ba85d2de4b2f6f0c655ec123de4313a3a
roderick-whang <56379137+roderick-whang@users.noreply.github.com>
2020-10-19 14:13:02 -0400

    updated

--- project2/project2.py
+++ project2/project2.py
@@ -30,17 +30,17 @@ def bdfs(maze, alg):
         raise Exception('Incorrect alg! Need BFS or DFS!')
     ##### Your implementation goes here. #####
     elif alg == 'BFS':
-        route = Queue() #create an empty queue object to process maze
+        route = Queue() # create an empty queue object to process maze
         pass
     elif alg == 'DFS':
-        route = Stack() #use stack object to process maze
+        route = Stack() # use stack object to process maze
         #initialize the route with a list of 3 "None" elements
         pass
 
-
-        #initialize the maze
     #print("initialize all vertex in the adjList")
     #print(maze.adjList)
+
+    #initialize the maze
     for vertex in maze.adjList:
         vertex.visited = False
         vertex.prev = None
@@ -57,17 +57,18 @@ def bdfs(maze, alg):
     #visit every possible vertices
 
     while route.isEmpty() is False:
-        current = route.pop()
+        current = route.pop() # pop from queus/stack
         #print("current position",current)
         #print("neighbor for current",current.neigh)
         for neigh in current.neigh:
             if neigh.visited is False:
-                if route.isFull():
-                    route.resize()
+                if route.isFull(): # checking queue/stack is full
+                    route.resize() # resize queue/stack if it is full
                 neigh.visited = True
                 neigh.dist = current.dist +1
                 route.push(neigh)
                 neigh.prev = current
+
                 #print("rank of neighbor",neigh.rank)
                 #print("previous rank of this neighbor",neigh.prev)
         #print("end of this position", current)
@@ -76,12 +77,13 @@ def bdfs(maze, alg):
     #print("Now checking the path")
     #dist = maze.exit.dist
 
-    #perform backward selection to find the start -> exit path!
+    # perform backward selection to find the start->exit path!
     backward = maze.exit
     maze.path = [backward.rank] + maze.path
     while backward.dist !=0:
-        maze.path = [backward.prev.rank]+maze.path#only put rank number to the list of our path
+        maze.path = [backward.prev.rank]+maze.path #only put rank number to the list of our path
         backward = backward.prev
+
         #print(maze.path)
         #print(backward.rank)
         #print(backward.neigh)
@@ -90,9 +92,8 @@ def bdfs(maze, alg):
         #print(backward.prev)
         #print(" ")
 
-
     return maze.path
-    ##### Your implementation goes here. #####
+
 
 """
 Main function.
