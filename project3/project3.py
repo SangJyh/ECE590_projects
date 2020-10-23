9d5f07f264c7f26a8c34e2ff285b91b8714b620d
SangJyh <sangjyh.lin@duke.edu>
2020-10-23 16:03:32 -0400

    update project3 (should work well)

--- project3/project3.py
+++ project3/project3.py
@@ -19,13 +19,40 @@ detectArbitrage
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
     ##### Your implementation goes here. #####
-<<<<<<< HEAD
     #bellmondford here
+    v = len(adjList)#n = # of vertex
+    adjList[0].dist = 0
+    tol = 1e-15
+    negcyc=None
+    # Bellman-Ford method
+    for i in range(v-1):#normal bellman ford (run v-1 loops)
+        for vertex in adjList:#for every vertex
+            for neighbor in vertex.neigh:#loop through all neighbor for this vertex
+                if vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol < neighbor.dist: #vertex's cost + edge cost < neighbor's current cost -> update neighbor's "cost and prev"
+                    neighbor.dist = vertex.dist + adjMat[vertex.rank][neighbor.rank]
+                    neighbor.prev = vertex
     
     
-=======
->>>>>>> 29d7da8c80b5b8261c59b5a2b854c2652439fd9b
-    return []
+    for vertex in adjList:#for every vertex
+        for neighbor in vertex.neigh:#loop through all neighbor for this vertex
+            if vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol < neighbor.dist: #if we have new update
+                neighbor.dist = vertex.dist + adjMat[vertex.rank][neighbor.rank]
+                neighbor.prev = vertex
+                #print(neighbor)
+                negcyc = [neighbor] #save start vertex
+    
+    if negcyc:#if there's negative cycle
+        #searching backward to find the cycle
+        #i = 0
+        while (negcyc[0].prev.rank != negcyc[-1].rank):
+            negcyc = [negcyc[0].prev] + negcyc
+            #i+=1
+        negcyc = [negcyc[-1]] + negcyc
+        negCyc = negcyc.copy()
+        for i in range(len(negcyc)):
+            negCyc[i] = negcyc[i].rank
+        return negCyc
+    else: return []
     ##### Your implementation goes here. #####
 
 ################################################################################
@@ -36,7 +63,9 @@ rates2mat
 def rates2mat(rates):
     ##### Your implementation goes here. #####
     # Currently this only returns a copy of the rates matrix.
-    return [[R for R in row] for row in rates]
+    rtm = [[-math.log(R) for R in row] for row in rates]
+    #print(rtm)
+    return rtm
     ##### Your implementation goes here. #####
 
 """
